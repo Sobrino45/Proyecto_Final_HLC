e34b7d34633d550b96171014c098c821086fcc43
Sobrino45 <jesus.sobrino-sanchez@iesruizgijon.com>
2025-03-04 13:01:27 +0100

    Agregado archivo .gitignore

---
+++ src/main/java/com/example/Proyecto_Final_HLC/ProyectoFinalHlcApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Final_HLC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalHlcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalHlcApplication.class, args);
+	}
+
+}
